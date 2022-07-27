7d114de0a13328067b242d37255632dbd856364c
b2ns <eminemdsp@163.com>
2022-07-27 11:06:07 +0800

    fix(pack): copyPlugin

--- packages/pack/src/plugins/copyPlugin.js
+++ packages/pack/src/plugins/copyPlugin.js
@@ -2,17 +2,19 @@ import { copyFileSync, existsSync, mkdirSync } from 'fs';
 import { dirname } from 'path';
 import { ensureArray } from '../utils.js';
 
-export default (_, opts) => {
+export default (events, opts) => {
   if (!opts) {
     return;
   }
-  opts = ensureArray(opts);
-  for (const opt of opts) {
-    const { from, to } = opt;
-    const toDir = dirname(to);
-    if (!existsSync(toDir)) {
-      mkdirSync(toDir);
+  events.on('end', () => {
+    opts = ensureArray(opts);
+    for (const opt of opts) {
+      const { from, to } = opt;
+      const toDir = dirname(to);
+      if (!existsSync(toDir)) {
+        mkdirSync(toDir);
+      }
+      copyFileSync(from, to);
     }
-    copyFileSync(from, to);
-  }
+  });
 };
