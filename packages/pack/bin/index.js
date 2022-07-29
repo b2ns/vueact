7eb39e0a4b1aa0e93d871280cc5adcb989eb0bc1
b2ns <eminemdsp@163.com>
2022-07-29 18:22:32 +0800

    fix(pack): read wrong config

--- packages/pack/bin/index.js
+++ packages/pack/bin/index.js
@@ -8,13 +8,13 @@ const Args = parseArgs(process.argv.slice(2));
 let config = null;
 
 if (Args.config) {
-  config = await import(Args.config);
+  config = (await import(Args.config)).default;
 } else {
   let dir = process.cwd();
   while (dir && dir !== '/') {
     const configFile = join(dir, 'pack.config.js');
     if (existsSync(configFile)) {
-      config = await import(configFile);
+      config = (await import(configFile)).default;
       break;
     }
     dir = dirname(dir);
