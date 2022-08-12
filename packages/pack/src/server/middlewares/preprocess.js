cc3e3b84f74e8c2c9ba6a18880152957ce635e9e
b2ns <eminemdsp@163.com>
2022-08-12 18:46:27 +0800

    style: preprocess

--- packages/pack/src/server/middlewares/preprocess.js
+++ packages/pack/src/server/middlewares/preprocess.js
@@ -1,22 +1,20 @@
-export default () => {
-  return (req, res, next) => {
-    Object.defineProperty(req, 'URL', {
-      get() {
-        return new URL(req.url, `http://${req.headers.host}`);
-      },
+export default () => (req, res, next) => {
+  Object.defineProperty(req, 'URL', {
+    get() {
+      return new URL(req.url, `http://${req.headers.host}`);
+    },
+  });
+
+  const parsed = req.URL;
+  const pathname = parsed.pathname || '/';
+
+  if (pathname === '/') {
+    res.writeHead(302, {
+      Location: `/index.html${parsed.search}${parsed.hash}`,
     });
+    res.end();
+    return;
+  }
 
-    const parsed = req.URL;
-    const pathname = parsed.pathname || '/';
-
-    if (pathname === '/') {
-      res.writeHead(302, {
-        Location: `/index.html${parsed.search}${parsed.hash}`,
-      });
-      res.end();
-      return;
-    }
-
-    next();
-  };
+  next();
 };
